c89e242b6e6a7e26774cac07fafbade13a0e5753
leuy1310 <uykyoto@icloud.com>
2023-07-23 21:32:13 +0700

    commit

---
+++ src/components/MyComponets.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MyComponets extends React.Component {
+    
+    // JSX
+    render() {
+        return (
+            <div>Hello Javascript</div>
+        );
+    }
+}
+
+export default MyComponets;
