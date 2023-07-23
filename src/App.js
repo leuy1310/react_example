04aab4385be078748e315b7a9e310bd7ff8975b3
leuy1310 <uykyoto@icloud.com>
2023-07-23 21:43:27 +0700

    state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponets from './components/MyComponets'
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react';
 
-  return (
-    <div className="App">
+
+class App extends React.Component {
+
+  render() {
+    return(
+      <div className="App">
       <MyComponets/>
-      {Math.random()}
     </div>
-  );
+    )
+  }
 }
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+    
+//   );
+// }
 
 export default App;
--- src/components/MyComponets.js
+++ src/components/MyComponets.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 class MyComponets extends React.Component {
+
+    // state
+    state = {
+        name: "ThomNT9",
+        address: "Kyoto",
+        age: 26
+    }
     
     // JSX
     render() {
         return (
-            <div>Hello Javascript</div>
+            <div>My name is {this.state.name} and I am from {this.state.address}</div>
         );
     }
 }
