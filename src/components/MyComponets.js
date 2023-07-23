5cec59285ea1e64423476b838a66a141c3e63092
leuy1310 <uykyoto@icloud.com>
2023-07-23 22:40:47 +0700

    set state

--- src/components/MyComponets.js
+++ src/components/MyComponets.js
@@ -7,12 +7,28 @@ class MyComponets extends React.Component {
         name: "ThomNT9",
         address: "Kyoto",
         age: 26
+    };
+
+    handleClick(event) {
+        console.log("Hello UyNL");
+        console.log(this.state.name);
+
+        this.setState({
+            name: "Eric Cantona"
+        })
+    }
+
+    hanldeOnMoverOver(e) {
+        console.log(e);
     }
     
     // JSX
     render() {
         return (
-            <div>My name is {this.state.name} and I am from {this.state.address}</div>
+            <div>
+                <div>My name is {this.state.name} and I am from {this.state.age}</div>
+                <button onClick={(event) => {this.handleClick(event)}}>Click Me</button>
+            </div>
         );
     }
 }
